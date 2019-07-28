abede1b2eb8f9028ffde80e98b44b8b7f8e7d8d1
chromatic <chromatic@wgz.org>
2019-07-28 09:19:56 -0700

    Update copyright and pragmas.

--- lib/Mail/SimpleList.pm
+++ lib/Mail/SimpleList.pm
@@ -1,9 +1,11 @@
 package Mail::SimpleList;
 
 use strict;
+use warnings;
+
 my $pod = do { local $/; <DATA> };
 
-use base 'Mail::Action';
+use parent 'Mail::Action';
 use Carp 'croak';
 
 use Mail::Mailer;
@@ -580,6 +582,6 @@ sending)
 
 =head1 COPYRIGHT
 
-Copyright (c) 2016 chromatic.  All rights reserved.  This module is distributed
-under the same terms as Perl 5.24, in the hope that it is useful but under no
-warranty.
+Copyright (c) 2003 - 2016 chromatic. All rights reserved. This module is
+distributed under the same terms as Perl 5.30, in the hope that it is useful
+but under no warranty.
